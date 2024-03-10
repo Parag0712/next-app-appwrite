17d3985637d3dc1e6463f412f98e699e3d32917f
Parag0712 <paragvadgama123@gmail.com>
2024-03-10 12:59:09 +0530

    add update profile

--- src/appwrite/authService.ts
+++ src/appwrite/authService.ts
@@ -11,6 +11,7 @@ export class AuthService {
         this.account = new Account(this.client);
     }
 
+    
     async createAccount({ email, password, name }: User) {
         try {
             const userAccount = await this.account.create(ID.unique(), email, password, name);
@@ -19,7 +20,6 @@ export class AuthService {
             } else {
                 return userAccount
             }
-
         } catch (error) {
             throw error;
         }
@@ -30,6 +30,23 @@ export class AuthService {
             const userAccount = await this.account.createEmailSession(email, password);
             return userAccount
         } catch (error: any) {
+            throw error;
+        }
+    }
+
+    async updateProfile({ name, email, password }:User) {
+        try {
+            // Update name if provided
+            if (name) {
+                const nameUpdateResult = await this.account.updateName(name, password);
+            }
+            // Update email if provided
+            if (email) {
+                const emailUpdateResult = await this.account.updateEmail(email, password);
+            }
+            return true;
+        } catch (error) {
+            console.error('Error updating profile:', error);
             throw error;
         }
     }
