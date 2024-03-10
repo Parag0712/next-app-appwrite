f331df0b3b92fdd3ac9a46c9b806feff80855d28
Parag0712 <paragvadgama123@gmail.com>
2024-03-10 13:21:31 +0530

    auth context added

--- src/context/authContext.ts
+++ src/context/authContext.ts
@@ -1,16 +1,25 @@
-import { createContext } from "react";
+import { createContext, useContext } from "react";
 
 type context = {
     authStatus: boolean,
+    user: Object | null;
     setAuthStatus: (status: boolean) => void
+    setUser: (user: Object | null) => void;
 }
 
 export const AuthContext = createContext<context>(
     {
         authStatus: false,
-        setAuthStatus: () => { }
+        user: null,
+        setAuthStatus: () => { },
+        setUser: () => {},
     }
 )
+
+export const useAuth = () => {
+    const data = useContext(AuthContext);
+    return data
+}
 
 export const AuthService = AuthContext.Provider;
 export default AuthContext;
